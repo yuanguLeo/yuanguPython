91579f6a209846b8bc3c39b3b945d8ac9e23d0c4
yuanguLeo <difree2008@126.com>
2019-08-15 23:05:01 +0800

    新增海龟绘图

---
+++ CodeDemo/shangxuetang/Turtle/1、图形化程序设计.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Time : 2019/8/15 22:38
+
+import turtle #导入turtle模块
+
+turtle.showturtle() #显示箭头
+turtle.write("元古") #写字符串
+turtle.forward(300) #前进300像素
+turtle.color("red") #画笔颜色改为red
+turtle.left(90) #箭头左转90度
+turtle.forward(100)
+turtle.goto(0,50) #去坐标(0,50)
+turtle.goto(0,0)
+turtle.penup() #抬笔
+turtle.goto(0,150)
+turtle.pendown() #下笔，这样，路径就会画出来
+turtle.circle(100) #画圆
+
+
+
+
+
+turtle.done()
---
+++ CodeDemo/shangxuetang/守破离.py
@@ -0,0 +1,2 @@
+#程序员修炼手册
+#http://www.bjsxt.com/xiulian.html
