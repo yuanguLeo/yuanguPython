a6bcb81cfe0ecb32d7198e3b48e092a9f5bd7b83
difree2008@126.com <difree2008@126.com>
2019-10-29 09:58:45 +0800

    更新尚学堂python一期练习

--- CodeDemo/shangxuetang/二期/异常.py
+++ CodeDemo/shangxuetang/二期/异常.py
@@ -2,3 +2,18 @@
 # -*- coding: utf-8 -*-
 # @Time : 2019/10/24 8:45
 
+# 示例：循环输入数字，如果不是数字则处理异常，直到输入88，则结果循环
+
+while True:
+    try:
+        i = int(input("请输入数字："))
+        if i == 88:
+            print("退出循环")
+            break
+    except BaseException as e:
+        print(e)
+
+
+
+
+
