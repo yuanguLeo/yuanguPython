d56b3c5f088606bceda83d0581bfb6305af36f49
yuangu <difree2008@126.com>
2020-03-18 17:19:23 +0800

    一起啃书

--- CodeDemo/TogetherNavigating/第2章/name_cases.py
+++ CodeDemo/TogetherNavigating/第2章/name_cases.py
@@ -22,6 +22,25 @@ print(a.upper())
 #首字母大写
 print(a.title())
 
+'''
+2-5需求：打印名人名句，包括引号
+'''
+print('Albrt Einstein said，"A person who never made a mistake never tried anything new"')
 
+'''
+2-6需求：将2-5中的名人姓名存在一个变量中，将显示的消息存在另一个变量中，打印
+'''
+famous_person = 'Einstein'
+message = '"A person who never made a mistake never tried anything new"'
+print('Albrt '+famous_person+' said, '+message)
+
+'''
+2-7需求：剔除名人中的空白
+'''
+name = ' 元古\t'
+print(name)
+print(name.lstrip())  #去除后空格
+print(name.rstrip())  #去除前空格
+print(name.strip())   #去除前后空格
 
 
