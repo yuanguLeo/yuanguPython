1256c1453edf3972d31e7081906960b46998e6cd
yuangu <difree2008@126.com>
2022-01-26 18:25:30 +0800

    一起啃书

--- CodeDemo/turtle/第十九课 我的地盘听我的/HomeWordNineteen.py
+++ CodeDemo/turtle/第十九课 我的地盘听我的/HomeWordNineteen.py
@@ -81,33 +81,33 @@
 
 # 1.编写一个函数，分别统计出传入字符串参数（可能不只一个参数）的英文字母、空格、数字和其它字符的个数。
 # 程序执行结果如图：
-def strs(var):
-    ''''分别统计传入的字符串的参数个数'''
-    symbols = r'''`!@#$%^&*()_+-=/*{}[]\|'";:/?,.<>'''
-    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    nums = '0123456789'
-    x = y = z = l =0
-
-    for i in var:
-        if i in symbols:
-            # 判断特殊字符
-            x += 1
-
-        elif i in chars:
-            # 判断字母
-            y += 1
-
-        elif i in nums:
-            # 判断数字
-            z += 1
-
-        elif i == " ":
-            # 判断空格
-            l += 1
-
-    print('英文字母：{0}个,空格：{1}个，数字：{2}个，特殊字符：{3}个'.format(y,l,z,x))
-
-strs('dd 1w2 @!')
+# def strs(var):
+#     ''''分别统计传入的字符串的参数个数'''
+#     symbols = r'''`!@#$%^&*()_+-=/*{}[]\|'";:/?,.<>'''
+#     chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+#     nums = '0123456789'
+#     x = y = z = l =0
+#
+#     for i in var:
+#         if i in symbols:
+#             # 判断特殊字符
+#             x += 1
+#
+#         elif i in chars:
+#             # 判断字母
+#             y += 1
+#
+#         elif i in nums:
+#             # 判断数字
+#             z += 1
+#
+#         elif i == " ":
+#             # 判断空格
+#             l += 1
+#
+#     print('英文字母：{0}个,空格：{1}个，数字：{2}个，特殊字符：{3}个'.format(y,l,z,x))
+#
+# strs('dd 1w2 @!')
 
 
 # 2.请写下这一节课你学习到的内容：格式不限，回忆并复述是加强记忆的好方式！
