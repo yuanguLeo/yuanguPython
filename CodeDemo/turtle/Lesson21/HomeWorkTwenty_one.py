262264328201332d4aa2a8d7a667765ba3c87c1c
yuangu <difree2008@126.com>
2022-01-27 16:56:27 +0800

    小甲鱼python入门

--- CodeDemo/turtle/Lesson21/HomeWorkTwenty_one.py
+++ CodeDemo/turtle/Lesson21/HomeWorkTwenty_one.py
@@ -3,21 +3,40 @@
 # @Time : 2022/1/27 15:28
 
 '''
-第二十一课：lambda表达式
+第二十一课：lambda(匿名函数)表达式
 '''
 
 # 0. 请使用lambda表达式将下边函数转变为匿名函数？
 # def fun_A(x, y=3):
 #         return x * y
 
+# g = lambda x,y : x*y
+# print(g(3,4))
+
 # 1. 请将下边的匿名函数转变为普通的屌丝函数？
 # lambda x : x if x % 2 else None
+# def num(x):
+#     if x :
+#         return x%2
+#     else:
+#         None
+# print(num(5))
 
 # 2. 感受一下使用匿名函数后给你的编程生活带来的变化？
 
 
 # 3. 你可以利用filter()和lambda表达式快速求出100以内所有3的倍数吗？
-
+# filter():把非True的值过滤掉
+# def nums():
+#     list = []
+#     for i in range(1,100):
+#         if i%3 == 0:
+#             list.append(i)
+#     print(list)
+#     print('共{0}个数字'.format(len(list)))
+# nums()
+# show = list(filter(lambda x : not(x%3),range(1,100)))
+# print(show)
 
 # 4. 还记得列表推导式吗？完全可以使用列表推导式代替filter()和lambda组合，你可以做到吗？
 
@@ -31,11 +50,11 @@
 
 
 # 6. 请目测以下表达式会打印什么？
-# def make_repeat(n):
-#         return lambda s : s * n
+def make_repeat(n):
+        return lambda s : s * n
 
-# double = make_repeat(2)
-# print(double(8))
-# print(double('FishC'))
+double = make_repeat(2)
+print(double(8))
+print(double('FishC'))
 
 
